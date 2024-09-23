243b6aaf4a3ca055597bd800a7de34e0ce875bfe
JeffersonGudiel <jeffersongudiel@icloud.com>
2024-09-22 21:30:30 -0600

    Additional configuration to the next file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["@ui"] = path.join(__dirname, "ruta/a/ui"); // Ajusta la ruta según sea necesario
+    return config;
+  },
+};
 
 export default nextConfig;
